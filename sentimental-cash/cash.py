55bd238e7a35408fd6c1c4b00520044e4aaeb6e0
Marinatsw <112609494+Marinatsw@users.noreply.github.com>
2023-02-05 13:22:38 +0200

    adding py

---
+++ sentimental-cash/cash.py
@@ -0,0 +1,32 @@
+from cs50 import get_float
+
+while True:
+    cents = get_float("Change: ")
+    if cents > 0:
+        break
+
+cents = round(cents*100)
+
+count = 0
+
+# Quarters
+while cents >= 25:
+    cents = cents -25
+    count +=1
+
+# Dimes
+while cents >= 10:
+    cents = cents - 10
+    count +=1
+
+# Nickels
+while cents >= 5:
+    cents = cents - 5
+    count +=1
+
+# Pennies
+while cents >= 1:
+    cents = cents - 1
+    count +=1
+
+print("Total coins: ", count)
